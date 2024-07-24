af84d92b2e58694c78eca4d2ac5b5b90db3430a4
sameersami07 <166928196+sameersami07@users.noreply.github.com>
2024-07-24 10:35:28 +0530

    primitive.java

---
+++ primitive.java
@@ -0,0 +1,21 @@
+public class primitive {
+    public static void main(String[] args) {
+        
+        byte myByte = 100;
+        short myShort = 1000;
+        int myInt = 100000;
+        long myLong = 1000000000L; 
+        float myFloat = 3.14f;  
+        double myDouble = 3.14159;
+        char myChar = 'A';
+        boolean myBoolean = true;
+        System.out.println("byte: " + myByte);
+        System.out.println("short: " + myShort);
+        System.out.println("int: " + myInt);
+        System.out.println("long: " + myLong);
+        System.out.println("float: " + myFloat);
+        System.out.println("double: " + myDouble);
+        System.out.println("char: " + myChar);
+        System.out.println("boolean: " + myBoolean);
+    }
+}
